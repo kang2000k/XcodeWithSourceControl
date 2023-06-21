b3ae81c60e733b39f2d452675b1d75bb4b6950c4
kang2000k <100081640+kang2000k@users.noreply.github.com>
2023-06-21 12:57:59 +0800

    Initial Commit

---
+++ XcodeWithSourceControl/XcodeWithSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeWithSourceControlApp.swift
+//  XcodeWithSourceControl
+//
+//  Created by 李文康 on 21/6/23.
+//
+
+import SwiftUI
+
+@main
+struct XcodeWithSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
